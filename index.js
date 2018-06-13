2cfae56baa1489d27c80e5252f10a7501bee569c
Samreen Islam <samreen.sfi@gmail.com>
2018-06-13 04:28:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase();
 }
-function shout(string){
-  return string.toUpperCase();
+function logShout(string){
+  console.log(string.toUpperCase());
 }
