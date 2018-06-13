e3d9f75137cb66f80180b9fd366f4932ac478eb0
Samreen Islam <samreen.sfi@gmail.com>
2018-06-13 04:34:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  
+  if(string === lowercase){
+    return "I can't hear you!";
+  }
+  if (string === uppercase){
+    return "YES INDEED!";
+  }
+  if (string === "I love you, Grandma"){
+    
+  }
 }
